4c2a3707573e9d8973c7c96083c4d07734a6ead4
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-07-20 19:01:51 -0700

    file path correction

--- components/UpdateListingModal.js
+++ components/UpdateListingModal.js
@@ -1,7 +1,7 @@
 import { Modal, Input, useNotification } from "web3uikit"
 import { useState, useEffect } from "react"
 import { useWeb3Contract } from "react-moralis"
-import nftMarketplaceAbi from "../constants/NFTMarketplace.json"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import { ethers } from "ethers"
 
 export default function UpdateListingModal({
